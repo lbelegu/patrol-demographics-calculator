25ee0b0a0530698433b72707f6a5e95074a19c96
Julia Healey-Parera <jh884@duke.edu>
2026-01-03 09:51:14 -0500

    src links and dates added to cities.js

--- src/cities.js
+++ src/cities.js
@@ -4,70 +4,81 @@ export const CITIES = [
         name: "Raleigh, NC",
         file: "NC/raleigh.geojson",
         lat: 35.7796,
-        lng: -78.6382
+        lng: -78.6382, 
+        src: "https://data-ral.opendata.arcgis.com/datasets/a8cd851199cc46029b6b73151711ab75_0/explore?location=35.828283%2C-78.644874%2C10.28",
+        date: "2026-01-03"
     },
     {
         id: 'charlotte-nc',
         name: "Charlotte, NC",
         file: "NC/charlotte.geojson",
         lat: 35.2271,
-        lng: -80.8431
+        lng: -80.8431, 
+        src: "https://data.charlottenc.gov/datasets/charlotte::cmpd-police-divisions-1/about",
+        date: "2026-01-03"
     },
     {
         id: 'los-angeles-ca',
         name: "Los Angeles, CA",
         file: "CA/los_angeles.geojson",
         lat: 34.0522,
-        lng: -118.2437
+        lng: -118.2437, 
+        src: "https://geohub.lacity.org/datasets/lapd-divisions",
+        date: "2026-01-03"
     },
     {
         id: 'sacramento-ca',
         name: "Sacramento, CA",
         file: "CA/sacramento.geojson",
         lat: 38.5816,
-        lng: -121.4944
+        lng: -121.4944, 
+        src: "https://data.saccounty.gov/datasets/sacramentocounty::sheriff-districts-1/explore?location=38.339142%2C-121.359043%2C9.24",
+        date: "2026-01-03"
     },
     {
         id: 'chicago-il',
         name: "Chicago, IL",
         file: "IL/chicago.geojson",
         lat: 41.8781,
-        lng: -87.6298
+        lng: -87.6298, 
+        src: "https://data.cityofchicago.org/Public-Safety/Boundaries-Police-Districts-current-/fthy-xz3r",
+        date: "2026-01-03"
     },
     {
         id: 'atlanta-ga',
         name: "Atlanta, GA",
         file: "GA/atlanta.geojson",
         lat: 33.7501,
-        lng: -84.3885
+        lng: -84.3885, 
+        src: "https://opendata.atlantaregional.com/datasets/coaplangis::atlanta-police-department-zones-2019/about",
+        date: "2026-01-03"
     },
     { 
         id: 'philadelphia-pa', 
         name: "Philadelphia, PA", 
         file: "PA/philadelphia.geojson", 
         lat: 40.0094, 
-        lng: -75.1333 
+        lng: -75.1333, 
+        src: "https://opendataphilly.org/datasets/police-districts/",
+        date: "2026-01-03"
     },
     { 
         id: 'arlington-tx', 
         name: "Arlington, TX", 
         file: "TX/arlington.geojson", 
         lat: 32.7007, 
-        lng: -97.1247 
-    },
-    { 
-        id: 'austin-tx', 
-        name: "Austin, TX", 
-        file: "TX/austin.geojson", 
-        lat: 30.3039, 
-        lng: -97.7544 
+        lng: -97.1247, 
+        src: "https://opendata.arlingtontx.gov/datasets/arlington-police-districts/explore",
+        date: "2026-01-03" 
     },
     { 
         id: 'denver-co', 
         name: "Denver, CO", 
         file: "CO/denver.geojson",
         lat: 39.7619, 
-        lng: -104.8811 
+        lng: -104.8811, 
+        src: "https://opendata-geospatialdenver.hub.arcgis.com/datasets/202b2bb479034e839a7587caa84be3f6_367/explore?location=39.727844%2C-104.921921%2C10.56",
+        date: "2026-01-03" 
     },
     
 ];
