032d0c95030502e5b20cb5411afd337408ce2fb4
Leon Belegu <lbelegu000@citymail.cuny.edu>
2026-01-22 18:12:06 -0500

    removed a header from homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -68,7 +68,7 @@ export default function Home() {
                         <h1 className="text-4xl md:text-5xl font-bold mb-16 text-white drop-shadow-md">
                             Patrol Demographics Calculator
                         </h1>
-                        <h2 className="text-3xl font-semibold text-white mb-4 mx-auto max-w-2xl">Mission Statement</h2>
+                        {/* <h2 className="text-3xl font-semibold text-white mb-4 mx-auto max-w-2xl">Mission Statement</h2> */}
                         <p className="text-white leading-relaxed text-lg mx-auto max-w-2xl">
                             Understanding the demographics of police districts is crucial for transparency,
                             equitable policing, and community trust. This tool aggregates open data to visualize
