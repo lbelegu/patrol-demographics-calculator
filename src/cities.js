9da265f2b533ab77f8fcc3dd3aadd64781eeb275
Julia Healey-Parera <jh884@duke.edu>
2026-03-02 13:57:54 -0500

    Removed duplicates and fixed formatting in cities.js

--- src/cities.js
+++ src/cities.js
@@ -1,173 +1,104 @@
 export const CITIES = [
     {
-        id: 'raleigh-nc',
-        name: "Raleigh, NC",
-        file: "NC/raleigh.geojson",
-        lat: 35.7796,
-        lng: -78.6382,
-        src: "https://data-ral.opendata.arcgis.com/datasets/a8cd851199cc46029b6b73151711ab75_0/explore?location=35.828283%2C-78.644874%2C10.28",
-        added_date: "2026-01-03",
-        source_date: "2025-10-09"
+        "id": 'raleigh-nc',
+        "name": "Raleigh, NC",
+        "file: "NC/raleigh.geojson",
+        "lat": 35.7796,
+        "lng": -78.6382,
+        "src": "https://data-ral.opendata.arcgis.com/datasets/a8cd851199cc46029b6b73151711ab75_0/explore?location=35.828283%2C-78.644874%2C10.28",
+        "added_date": "2026-01-03",
+        "source_date": "2025-10-09"
     },
     {
-        id: 'charlotte-nc',
-        name: "Charlotte, NC",
-        file: "NC/charlotte.geojson",
-        lat: 35.2271,
-        lng: -80.8431,
-        src: "https://data.charlottenc.gov/datasets/charlotte::cmpd-police-divisions-1/about",
-        added_date: "2026-01-03",
-        source_date: "2024-12-04"
+        "id": 'charlotte-nc',
+        "name": "Charlotte, NC",
+        "file": "NC/charlotte.geojson",
+        "lat": 35.2271,
+        "lng": -80.8431,
+        "src": "https://data.charlottenc.gov/datasets/charlotte::cmpd-police-divisions-1/about",
+        "added_date": "2026-01-03",
+        "source_date": "2024-12-04"
     },
     {
-        id: 'los-angeles-ca',
-        name: "Los Angeles, CA",
-        file: "CA/los_angeles.geojson",
-        lat: 34.0522,
-        lng: -118.2437,
-        src: "https://geohub.lacity.org/datasets/lapd-divisions",
-        added_date: "2026-01-03",
-        source_date: "2023-04-28"
+        "id": 'los-angeles-ca',
+        "name": "Los Angeles, CA",
+        "file": "CA/los_angeles.geojson",
+        "lat": 34.0522,
+        "lng": -118.2437,
+        "src": "https://geohub.lacity.org/datasets/lapd-divisions",
+        "added_date": "2026-01-03",
+        "source_date": "2023-04-28"
     },
     {
-        id: 'sacramento-ca',
-        name: "Sacramento, CA",
-        file: "CA/sacramento.geojson",
-        lat: 38.5816,
-        lng: -121.4944,
-        src: "https://data.saccounty.gov/datasets/sacramentocounty::sheriff-districts-1/explore?location=38.339142%2C-121.359043%2C9.24",
-        added_date: "2026-01-03",
-        source_date: "2021-07-15"
+        "id": 'sacramento-ca',
+        "name": "Sacramento, CA",
+        "file": "CA/sacramento.geojson",
+        "lat": 38.5816,
+        "lng": -121.4944,
+        "src": "https://data.saccounty.gov/datasets/sacramentocounty::sheriff-districts-1/explore?location=38.339142%2C-121.359043%2C9.24",
+        "added_date": "2026-01-03",
+        "source_date": "2021-07-15"
     },
     {
-        id: 'chicago-il',
-        name: "Chicago, IL",
-        file: "IL/chicago.geojson",
-        lat: 41.8781,
-        lng: -87.6298,
-        src: "https://data.cityofchicago.org/Public-Safety/Boundaries-Police-Districts-current-/fthy-xz3r",
-        added_date: "2026-01-03",
-        source_date: "2024-12-02"
+        "id": 'chicago-il',
+        "name": "Chicago, IL",
+        "file": "IL/chicago.geojson",
+        "lat": 41.8781,
+        "lng": -87.6298,
+        "src": "https://data.cityofchicago.org/Public-Safety/Boundaries-Police-Districts-current-/fthy-xz3r",
+        "added_date": "2026-01-03",
+        "source_date": "2024-12-02"
     },
     {
-        id: 'atlanta-ga',
-        name: "Atlanta, GA",
-        file: "GA/atlanta.geojson",
-        lat: 33.7501,
-        lng: -84.3885,
-        src: "https://opendata.atlantaregional.com/datasets/coaplangis::atlanta-police-department-zones-2019/about",
-        added_date: "2026-01-03",
-        source_date: "2023-05-23"
+        "id": 'atlanta-ga',
+        "name": "Atlanta, GA",
+        "file": "GA/atlanta.geojson",
+        "lat": 33.7501,
+        "lng": -84.3885,
+        "src": "https://opendata.atlantaregional.com/datasets/coaplangis::atlanta-police-department-zones-2019/about",
+        "added_date": "2026-01-03",
+        "source_date": "2023-05-23"
     },
     {
-        id: 'philadelphia-pa',
-        name: "Philadelphia, PA",
-        file: "PA/philadelphia.geojson",
-        lat: 40.0094,
-        lng: -75.1333,
-        src: "https://opendataphilly.org/datasets/police-districts/",
-        added_date: "2026-01-03",
-        source_date: "2026-01-03" // update this
+        "id": 'philadelphia-pa',
+        "name": "Philadelphia, PA",
+        "file": "PA/philadelphia.geojson",
+        "lat": 40.0094,
+        "lng": -75.1333,
+        "src": "https://opendataphilly.org/datasets/police-districts/",
+        "added_date": "2026-01-03",
+        "source_date": "2026-01-03" 
     },
     {
-        id: 'arlington-tx',
-        name: "Arlington, TX",
-        file: "TX/arlington.geojson",
-        lat: 32.7007,
-        lng: -97.1247,
-        src: "https://opendata.arlingtontx.gov/datasets/arlington-police-districts/explore",
-        added_date: "2026-01-03",
-        source_date: "2023-05-25"
+        "id": 'arlington-tx',
+        "name": "Arlington, TX",
+        "file": "TX/arlington.geojson",
+        "lat": 32.7007,
+        "lng": -97.1247,
+        "src": "https://opendata.arlingtontx.gov/datasets/arlington-police-districts/explore",
+        "added_date": "2026-01-03",
+        "source_date": "2023-05-25"
     },
     {
-        id: 'denver-co',
-        name: "Denver, CO",
-        file: "CO/denver.geojson",
-        lat: 39.7619,
-        lng: -104.8811,
-        src: "https://opendata-geospatialdenver.hub.arcgis.com/datasets/202b2bb479034e839a7587caa84be3f6_367/explore?location=39.727844%2C-104.921921%2C10.56",
-        added_date: "2026-01-03",
-        source_date: "2026-01-03" // update this
-    },
-    {
-        id: 'austin-tx',
-        name: "Austin, TX",
-        file: "TX/austin.geojson",
-        lat: 30.3039,
-        lng: -97.7544,
-        src: "https://data.austintexas.gov/dataset/Austin-Police-Department-Districts/9jeg-fsk5",
-        added_date: "2026-01-03",
-        source_date: "2026-01-03", // update this
-    },
-    {
-        id: 'new_york_city-ny',
-        name: 'New York City, NY',
-        file: "NY/new_york_city.geojson",
-        lat: 40.6635,
-        lng: -73.9387,
-        src: "https://www.nyc.gov/content/planning/pages/resources/datasets/police-precincts",
-        added_date: "2026-01-07",
-        source_date: "2025-11-01"
-    },
-    {
-        id: "miami-fl",
-        name: "Miami, FL",
-        file: "FL/miami.geojson",
-        lat: 25.7824,
-        lng: -80.2278,
-        src: "https://datahub-miamigis.opendata.arcgis.com/datasets/18ea60e4964943a4981b60d800fe28e0_0/explore?location=25.782469%2C-80.227576%2C11",
-        added_date: "2026-02-10",
-        source_date: "2024-10-22"
+        "id": 'denver-co',
+        "name": "Denver, CO",
+        "file": "CO/denver.geojson",
+        "lat": 39.7619,
+        "lng": -104.8811,
+        "src": "https://opendata-geospatialdenver.hub.arcgis.com/datasets/202b2bb479034e839a7587caa84be3f6_367/explore?location=39.727844%2C-104.921921%2C10.56",
+        "added_date": "2026-01-03",
+        "source_date": "2026-01-03" 
     },
     {
 
-        id: "portland-or",
-        name: "Portland, OR",
-        file: "OR/portland.geojson",
-        lat: 45.5449,
-        lng: -122.646,
-        src: "https://gis-pdx.opendata.arcgis.com/datasets/PDX::police-districts-ppb/about",
-        district_field: "DISTRICT",
-        added_date: "2026-02-12",
-        source_date: "2024-09-12"
-
-    },
-    {
-
-        "id": "seattle-wa",
-        "name": "Seattle, WA",
-        "file": "WA/seattle.geojson",
-        "lat": 47.6137,
-        "lng": -122.3383,
-        "src": "The Seattle Police Department has different iterations of police beats as the city's needs evolved.  This layer represents what the beats looked like from 2017 to present.   This layer was published in the WGS 1984 Web Mercator Auxiliary Sphere coordinate system.",
-        "district_field": "first_precinct",
-        "added_date": "2026-02-16",
-        "source_date": "2025-02-27"
-
-    }, 
-    {
-        "id": "austin-tx",
-        "name": "Austin, TX",
-        "file": "TX/austin.geojson",
-        "lat": 30.3075,
-        "lng": -97.7486,
-        "src": "https://data.austintexas.gov/dataset/Austin-Police-Department-Districts/9jeg-fsk5",
-        "district_field": "sector_name",
-        "added_date": "2026-02-15",
-        "source_date": "2026-02-15"
-
-    },
-    {
-
-        "id": "tucson-az",
-        "name": "Tucson, AZ",
-        "file": "AZ/tucson.geojson",
-        "lat": 32.1557,
-        "lng": -110.8825,
-        "src": "https://azgeo-open-data-agic.hub.arcgis.com/datasets/PimaMaps::tucson-police-department-patrol-sectors/about",
-        "district_field": "TMSECT",
-        "added_date": "2026-02-17",
-        "source_date": "2021-02-24"
+        "id": 'new_york_city-ny',
+        "name": 'New York City, NY',
+        "file": "NY/new_york_city.geojson",
+        "lat": 40.6635,
+        "lng": -73.9387,
+        "src": "https://www.nyc.gov/content/planning/pages/resources/datasets/police-precincts",
+        "added_date": "2026-01-07",
+        "source_date": "2025-11-01"
 
     },
     {
@@ -198,32 +129,6 @@ export const CITIES = [
     },
     {
 
-        "id": "omaha-ne",
-        "name": "Omaha, NE",
-        "file": "NE/omaha.geojson",
-        "lat": 41.2919,
-        "lng": -96.0697,
-        "src": "https://data.dogis.org/datasets/051e9931ae0840f38742367fa560ac0e_0/about",
-        "district_field": "district_text",
-        "added_date": "2026-02-24",
-        "source_date": "2024-10-21"
-
-    },
-    {
-
-        "id": "virginia-beach-va",
-        "name": "Virginia Beach, VA",
-        "file": "VA/virginia_beach.geojson",
-        "lat": 36.7412,
-        "lng": -76.0741,
-        "src": "https://gis.data.vbgov.com/datasets/c7c01aa81a0c4ba6b3991a49cd0dde23_1/about",
-        "district_field": "PATROL_ZONE",
-        "added_date": "2026-02-24",
-        "source_date": "2023-03-13"
-
-    },
-    {
-
         "id": "san-diego-ca",
         "name": "San Diego, CA",
         "file": "CA/san_diego.geojson",
@@ -245,7 +150,7 @@ export const CITIES = [
         "src": "https://datahub-miamigis.opendata.arcgis.com/datasets/18ea60e4964943a4981b60d800fe28e0_0/explore?location=25.768175%2C-80.186260%2C12",
         "district_field": "FID",
         "added_date": "2026-03-02",
-        "source_date": "2026-03-02"
+        "source_date": "2024-10-22"
       
     },
     {
@@ -258,7 +163,7 @@ export const CITIES = [
         "src": "https://data.baltimorecity.gov/datasets/baltimore::police-districts-4/explore?location=39.284054%2C-76.618882%2C10",
         "district_field": "Dist_Name",
         "added_date": "2026-03-02",
-        "source_date": "2026-03-02"
+        "source_date": "2025-07-17"
       
     },
     {
@@ -271,7 +176,7 @@ export const CITIES = [
         "src": "https://data.detroitmi.gov/datasets/dpd-precincts/explore?location=42.352756%2C-83.099019%2C10",
         "district_field": "name",
         "added_date": "2026-03-02",
-        "source_date": "2026-03-02"
+        "source_date": "2020-11-06"
       
     },
     {
@@ -284,7 +189,7 @@ export const CITIES = [
         "src": "https://gis-pdx.opendata.arcgis.com/datasets/police-districts-ppb",
         "district_field": "DISTRICT",
         "added_date": "2026-03-02",
-        "source_date": "2026-03-02"
+        "source_date": "2024-09-12"
 
     },
     {
@@ -297,7 +202,7 @@ export const CITIES = [
         "src": "https://data.austintexas.gov/Public-Safety/Austin-Police-Department-Districts/9jeg-fsk5",
         "district_field": "patrol_area",
         "added_date": "2026-03-02",
-        "source_date": "2026-03-02"
+        "source_date": "2026-03-01"
 
     },
     {
@@ -310,7 +215,7 @@ export const CITIES = [
         "src": "https://data.seattle.gov/dataset/Seattle-Police-Department-Beats-Web-Mercator-/vhna-33bt/about_data",
         "district_field": "beat",
         "added_date": "2026-03-02",
-        "source_date": "2026-03-02"
+        "source_date": "2025-02-27"
 
     },
     {
@@ -323,7 +228,7 @@ export const CITIES = [
         "src": "https://data.indy.gov/datasets/indianapolis-police-zones/explore?location=39.771065%2C-86.107110%2C10",
         "district_field": "DISTRICT",
         "added_date": "2026-03-02",
-        "source_date": "2026-03-02"
+        "source_date": "2025-03-12"
 
     },
     {
@@ -336,7 +241,7 @@ export const CITIES = [
         "src": "https://azgeo-open-data-agic.hub.arcgis.com/datasets/PimaMaps::tucson-police-department-patrol-sectors/about",
         "district_field": "TMSECT",
         "added_date": "2026-03-02",
-        "source_date": "2026-03-02"
+        "source_date": "2021-02-24"
 
     },
     {
@@ -349,7 +254,7 @@ export const CITIES = [
         "src": "https://data.dogis.org/datasets/051e9931ae0840f38742367fa560ac0e_0/about",
         "district_field": "district_text",
         "added_date": "2026-03-02",
-        "source_date": "2026-03-02"
+        "source_date": "2024-10-21"
 
     },
     {
@@ -362,7 +267,7 @@ export const CITIES = [
         "src": "https://gis.data.vbgov.com/datasets/c7c01aa81a0c4ba6b3991a49cd0dde23_1/explore",
         "district_field": "PATROL_ZONE",
         "added_date": "2026-03-02",
-        "source_date": "2026-03-02"
+        "source_date": "2023-03-13"
 
     }
 ];
