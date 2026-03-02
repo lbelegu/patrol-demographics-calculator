f33a5f6ba973de79ff63131bea2534c612cc2032
JuliaHealeyParera <JuliaHealeyParera@users.noreply.github.com>
2026-03-02 18:27:52 +0000

    feat: add Omaha, NE

--- src/cities.js
+++ src/cities.js
@@ -338,6 +338,19 @@ export const CITIES = [
         "added_date": "2026-03-02",
         "source_date": "2026-03-02"
 
+    },
+    {
+
+        "id": "omaha-ne",
+        "name": "Omaha, NE",
+        "file": "NE/omaha.geojson",
+        "lat": 41.2919,
+        "lng": -96.0697,
+        "src": "https://data.dogis.org/datasets/051e9931ae0840f38742367fa560ac0e_0/about",
+        "district_field": "district_text",
+        "added_date": "2026-03-02",
+        "source_date": "2026-03-02"
+
     }
 ];
 
